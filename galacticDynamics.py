d53f3ba54dda70c15ef7e8d4c515095136e69ec4
Benjamin Guilfoyle <bguilfoyle@physstaff.nuim.ie>
2019-12-02 13:09:30 +0000

    stuff added

---
+++ galacticDynamics.py
@@ -0,0 +1,62 @@
+"""
+Author: Ben Guilfoyle - github.com/bengfoyle
+Overview: Calculating the gravitational efferty of the close passage of two
+glaactic nuclei on a disk of stars. Done as part of Maynooth University
+BSc Physics with Astrophysics, Module EP408.
+"""
+
+import numpy as np
+
+#===============================================================================
+def makePlot():
+    """
+    Overview: Make a plot of results
+    """
+    return
+#===============================================================================
+
+#===============================================================================
+def position():
+    """
+    Overview: Calcualte position of body
+    """
+    return pA,pB
+#===============================================================================
+
+#===============================================================================
+def distance():
+    """
+    Overview: Calculate distance between two bodies
+    """
+    return
+#===============================================================================
+
+#===============================================================================
+def acceleration(vA,vB):
+    """
+    Overview: Calcualte acceleration of a body
+    """
+    return (vA - vB) / dt
+#===============================================================================
+
+#===============================================================================
+def velocity(pA,pB):
+    """
+    Overview: Calcualte the velocity of a body
+    """
+    return (pA - pB) / dt
+#===============================================================================
+
+#===============================================================================
+def pytha(x1,x2,y1,y2,z1,z2):
+    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
+#===============================================================================
+
+#===============================================================================
+def simulation():
+    """
+    Overview: Run a simulation of galactic dynamics for a given system
+    """
+    return
+#===============================================================================
+simulation()
