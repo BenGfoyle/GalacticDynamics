c12d32d7aba49e77710eb39f9da70faa6f6552be
Benjamin Guilfoyle <bguilfoyle@physstaff.nuim.ie>
2019-12-02 13:42:16 +0000

    animate a point

--- matplotAnimation.py
+++ matplotAnimation.py
@@ -3,33 +3,34 @@ Author: Ben Guilfoyle - github.com/bengfoyle
 Overview: Create an animation using matplot.
 """
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import animation
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-line, = ax.plot([], [], lw=2)
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = 4,3
+from matplotlib.animation import FuncAnimation
 
-# initialization function: plot the background of each frame
-def init():
-    line.set_data([], [])
-    return line,
+r = 1 # radius of circle
+def circle(phi):
+    return np.array([r*np.cos(phi), r*np.sin(phi)])
 
-# animation function.  This is called sequentially
-def animate(i):
-    x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
+# create a figure with an axes
+fig, ax = plt.subplots()
+# set the axes limits
+ax.axis([-1.5,1.5,-1.5,1.5])
+# set equal aspect such that the circle is not shown as ellipse
+ax.set_aspect("equal")
+# create a point in the axes
+point, = ax.plot(0,1, marker="o")
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
+# Updating function, to be repeatedly called by the animation
+def update(phi):
+    # obtain point coordinates
+    x,y = circle(phi)
+    # set point's coordinates
+    point.set_data([x],[y])
+    return point,
 
-# save the animation as an mp4.  This requires ffmpeg or mencoder to be
-# installed.  The extra_args ensure that the x264 codec is used, so that
-# the video can be embedded in html5.  You may need to adjust this for
-# your system: for more information, see
-# http://matplotlib.sourceforge.net/api/animation_api.html
+# create animation with 10ms interval, which is repeated,
+# provide the full circle (0,2pi) as parameters
+ani = FuncAnimation(fig, update, interval=10, blit=True, repeat=True,
+                    frames=np.linspace(0,2*np.pi,360, endpoint=False))
 
 plt.show()
